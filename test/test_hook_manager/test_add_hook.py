142569da18f458eb3bad6d942bc9198c60bdefc3
hackerlikecomputer <hackerlikecomputer@gmail.com>
2023-05-30 16:23:18 -0500

    add new test

--- test/test_hook_manager/test_add_hook.py
+++ test/test_hook_manager/test_add_hook.py
@@ -11,16 +11,43 @@ def test_add_hook_hook_exists():
     then tests that the specified command was added to the hook
     """
     hook_contents = "#!/bin/sh\n\nexit 0\n"
+    command_name = "enforce_notebook_run_order"
+    command_text = f"python -m {command_name}"
     with TempGitRepo():
         # write the existing hook contents
         hook_path = os.path.join(".git", "hooks", "pre-commit")
         with open(hook_path, "w", encoding="UTF-8") as hook_file:
             hook_file.write(hook_contents)
 
+        assert command_text not in hook_contents
+
         # add the hook
-        add_hook("pre-commit", "enforce_notebook_run_order")
+        add_hook("pre-commit", command_name)
 
         # check that the hook was updated
         with open(hook_path, "r", encoding="UTF-8") as hook_file:
             new_hook_contents = hook_file.read()
-            assert "python -m enforce_notebook_run_order" in new_hook_contents
+            assert command_text in new_hook_contents
+
+
+def test_add_hook_hook_not_exists():
+    """
+    creates a temporary git repo and tests that the specified command was added to the hook
+    """
+    command_name = "enforce_notebook_run_order"
+    command_text = f"python -m {command_name}"
+    with TempGitRepo():
+        # check that the hook doesn't exist
+        hook_path = os.path.join(".git", "hooks", "pre-commit")
+        assert not os.path.exists(hook_path)
+
+        # add the hook
+        add_hook("pre-commit", command_name)
+
+        # check that the hook was created
+        assert os.path.exists(hook_path)
+
+        # check that the hook was updated
+        with open(hook_path, "r", encoding="UTF-8") as hook_file:
+            new_hook_contents = hook_file.read()
+            assert command_text in new_hook_contents
