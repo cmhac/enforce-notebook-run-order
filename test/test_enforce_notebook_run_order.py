cdd36ebd507ab0b2c1d4925b8e95fdcd3676288f
hackerlikecomputer <hackerlikecomputer@gmail.com>
2023-06-01 09:20:24 -0500

    update tests

--- test/test_enforce_notebook_run_order.py
+++ test/test_enforce_notebook_run_order.py
@@ -1,16 +1,20 @@
-"""tests the enforce_run_order script"""
+"""tests the enforce_notebook_run_order module"""
 
 import os
 import pytest
 import enforce_notebook_run_order
-from enforce_notebook_run_order import InvalidNotebookRunError
+from enforce_notebook_run_order import (
+    InvalidNotebookRunError,
+    NotebookCodeCellNotRunError,
+    NotebookRunOrderError,
+)
 
-# pylint: disable=W0621:redefined-outer-name
+# pylint: disable=redefined-outer-name
 
 
 @pytest.fixture
 def valid_notebook_data():
-    """returns valid test notebook json"""
+    """Returns valid test notebook json."""
     return {
         "cells": [
             {"cell_type": "code", "execution_count": 1},
@@ -22,7 +26,7 @@ def valid_notebook_data():
 
 @pytest.fixture
 def out_of_order_notebook_data():
-    """returns invalid test notebook json"""
+    """Returns invalid test notebook json."""
     return {
         "cells": [
             {"cell_type": "code", "execution_count": 1},
@@ -34,7 +38,7 @@ def out_of_order_notebook_data():
 
 @pytest.fixture
 def notebook_cell_not_run_data():
-    """returns invalid test notebook json"""
+    """Returns invalid test notebook json."""
     return {
         "cells": [
             {"cell_type": "code", "execution_count": 1},
@@ -45,19 +49,13 @@ def notebook_cell_not_run_data():
 
 
 def test_check_notebook_run_order_valid(valid_notebook_data):
-    """tests that valid notebook data does not raise an error"""
-    try:
-        enforce_notebook_run_order.check_notebook_run_order(valid_notebook_data)
-    except (
-        enforce_notebook_run_order.NotebookCodeCellNotRunError,
-        enforce_notebook_run_order.NotebookRunOrderError,
-    ):
-        pytest.fail("Valid notebook data raised error unexpectedly")
+    """Tests that valid notebook data does not raise an error."""
+    enforce_notebook_run_order.check_notebook_run_order(valid_notebook_data)
 
 
 def test_check_notebook_run_order_out_of_order(out_of_order_notebook_data):
-    """tests that out of order notebook data raises an error"""
-    with pytest.raises(enforce_notebook_run_order.NotebookRunOrderError) as error:
+    """Tests that out of order notebook data raises an error."""
+    with pytest.raises(NotebookRunOrderError) as error:
         enforce_notebook_run_order.check_notebook_run_order(out_of_order_notebook_data)
 
     expected_error_message = (
@@ -69,9 +67,9 @@ def test_check_notebook_run_order_out_of_order(out_of_order_notebook_data):
     assert str(error.value) == expected_error_message
 
 
-def test_notebook_cell_not_run(notebook_cell_not_run_data):
-    """tests that a notebook cell not run raises an error"""
-    with pytest.raises(enforce_notebook_run_order.NotebookCodeCellNotRunError) as error:
+def test_check_notebook_run_order_cell_not_run(notebook_cell_not_run_data):
+    """Tests that a notebook cell not run raises an error."""
+    with pytest.raises(NotebookCodeCellNotRunError) as error:
         enforce_notebook_run_order.check_notebook_run_order(notebook_cell_not_run_data)
 
     expected_error_message = (
@@ -83,19 +81,15 @@ def test_notebook_cell_not_run(notebook_cell_not_run_data):
 
 def test_check_all_repo_notebooks_valid(mocker):
     """
-    tests that check_notebook_run_order is called correctly
-    for each notebook in a given folder
+    Tests that check_notebook_run_order is called correctly
+    for each notebook in a given folder.
     """
-    # Adjust the patch path to include 'src' and use the correct module and function names.
     mock_check_notebook_run_order = mocker.patch(
         "enforce_notebook_run_order.check_notebook_run_order"
     )
-    mock_check_notebook_run_order.reset_mock()
 
     test_data_dir = os.path.join(
-        "test",
-        "test_data",
-        "enforce_notebook_run_order_valid",
+        "test", "test_data", "enforce_notebook_run_order_valid"
     )
 
     enforce_notebook_run_order.check_all_repo_notebooks(test_data_dir)
@@ -105,13 +99,11 @@ def test_check_all_repo_notebooks_valid(mocker):
 
 def test_check_all_repo_notebooks_invalid():
     """
-    tests that check_notebook_run_order is called correctly
-    for each notebook in a given folder
+    Tests that check_notebook_run_order raises InvalidNotebookRunError
+    for each notebook in a given folder.
     """
     test_data_dir = os.path.join(
-        "test",
-        "test_data",
-        "enforce_notebook_run_order_invalid",
+        "test", "test_data", "enforce_notebook_run_order_invalid"
     )
 
     with pytest.raises(InvalidNotebookRunError):
