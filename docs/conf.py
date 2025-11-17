31066ba7868bf79014e7f0b324047c39c3c77e74
Chris Hacker <christopher.hacker@washpost.com>
2025-11-17 15:56:50 -0500

    fix: add custom css to fix overlapping header items

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = []
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 html_theme_options = {
     "show_toc_level": 2,
